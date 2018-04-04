e41b5da60277e483a5fe71a1b5d23e5299ac4e58
chitrank13 <singh.chitrank@gmail.com>
2018-04-05 00:19:51 +0530

    repo setup

---
+++ header.php
@@ -0,0 +1,44 @@
+<style type="text/css">
+<!--
+body {
+	margin-left: 0px;
+	margin-top: 0px;
+}
+-->
+</style>
+  <tr>
+    <td width="90%" valign="top">
+<div align="left"><object classid=clsid:D27CDB6E-AE6D-11cf-96B8-444553540000
+codebase=http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,2,0
+width=500
+height=68>
+<param name=movie value=image/fence.swf>
+<param name=quality value=high>
+<param name=BGCOLOR value=#000000>
+<param name=SCALE value=showall>
+<param name=wmode value=transparent> 
+</object></div></td>
+</table>
+  <Table width="100%">
+  <tr>
+  <td>
+  <?php @$_SESSION['login']; 
+  error_reporting(1);
+  ?>
+  </td>
+    <td>
+	<?php
+	if(isset($_SESSION['login']))
+	{
+	 echo "<div align=\"right\"><strong><a href=\"index.php\"> Home </a>|<a href=\"signout.php\">Signout</a></strong></div>";
+	 }
+	 else
+	 {
+	 	echo "&nbsp;";
+	 }
+	?>
+	</td>
+	
+  </tr>
+  
+</table>
---
+++ index.php
@@ -0,0 +1,102 @@
+<?php
+session_start();
+?>
+<html>
+<head>
+<title>Wel come to Quiz game</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link href="quiz.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<?php
+include("header.php");
+include("database.php");
+extract($_POST);
+
+if(isset($submit))
+{
+	$rs=mysql_query("select * from mst_user where login='$loginid' and pass='$pass'");
+	if(mysql_num_rows($rs)<1)
+	{
+		$found="N";
+	}
+	else
+	{
+		$_SESSION[login]=$loginid;
+	}
+}
+if (isset($_SESSION[login]))
+{
+echo "<h1 class='style8' align=center>Wel come to Quiz game</h1>";
+		echo '<table width="28%"  border="0" align="center">
+  <tr>
+    <td width="93%" valign="bottom" bordercolor="#0000FF"> <a href="sublist.php" class="style4">Subject for Quiz </a></td>
+  </tr>
+  <tr>
+    <td valign="bottom"> <a href="result.php" class="style4">Result </a></td>
+  </tr>
+</table>';
+   
+		exit;
+		
+
+}
+
+
+?>
+<table width="100%" border="0">
+  <tr>
+    <td width="70%" height="25">&nbsp;</td>
+    <td width="1%" rowspan="2" bgcolor="#CC3300"><span class="style6"></span></td>
+    <td width="29%" bgcolor="#CC3333"><div align="center" class="style1">User Login </div></td>
+  </tr>
+  <tr>
+    <td height="296" valign="top"><div align="center">
+        <h1 class="style8">Wel come to Quiz game</h1>
+        <param name="movie" value="english theams two brothers.dat">
+        <param name="quality" value="high">
+        <param name="movie" value="Drag to a file to choose it.">
+        <param name="quality" value="high">
+        <param name="BGCOLOR" value="#FFFFFF">
+<p align="left" class="style5">&nbsp;</p>
+      <blockquote>
+          <p align="left" class="style5"><span class="style7">Wel Come to Quiz Game
+            . This  will provide the quiz on the basis of General Knowledge. 
+            You need to login for the take the Quiz game.</span></p>
+      </blockquote>
+    </div></td>
+    <td valign="top"><form name="form1" method="post" action="">
+      <table width="200" border="0">
+        <tr>
+          <td><span class="style2">Login ID </span></td>
+          <td><input name="loginid" type="text" id="loginid2"></td>
+        </tr>
+        <tr>
+          <td><span class="style2">Password</span></td>
+          <td><input name="pass" type="password" id="pass2"></td>
+        </tr>
+        <tr>
+          <td colspan="2"><span class="errors">
+            <?php
+		  if(isset($found))
+		  {
+		  	echo "Invalid Username or Password";
+		  }
+		  ?>
+          </span></td>
+          </tr>
+        <tr>
+          <td colspan=2 align=center class="errors">
+		  <input name="submit" type="submit" id="submit" value="Login">		  </td>
+        </tr>
+        <tr>
+          <td colspan="2" bgcolor="#CC3300"><div align="center"><span class="style4">New User ? <a href="signup.php">Signup Free</a></span></div></td>
+          </tr>
+      </table>
+    </form></td>
+  </tr>
+</table>
+
+</body>
+</html>
